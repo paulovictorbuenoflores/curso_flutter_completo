4f3de9e4af84bfbe12a632cb7e08afd5c7663bed
pv <paulovictorflores28@gmail.com>
2022-09-15 01:49:44 -0300

    for in percorrendo map

--- dart_estrutura_de_controle_22/controle/for.dart
+++ dart_estrutura_de_controle_22/controle/for.dart
@@ -1,8 +1,22 @@
+import 'dart:ffi';
+
 void main(List<String> args) {
   var notas = [10, 2.3, 3.4, 4.2, 1.5];
 
 //for in
   for (var nota in notas) {
     print(nota);
+  }
+  print("\n\n");
+  //percorrendo um Map
+  Map<String, double> alunos = {
+    'Joao': 91,
+    'Maria': 4.5,
+    'Ana': 5.6,
+    'Joana': 8.7
+  };
+
+  for (var nome in alunos.keys) {
+    print("Aluno ${nome} nota: ${alunos[nome]}");
   }
 }
