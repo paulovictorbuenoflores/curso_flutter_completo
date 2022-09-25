0af7c707b30fe89710d6dc733d94155893c15ada
pv <paulovictorflores28@gmail.com>
2022-09-25 03:25:04 -0300

    estrutura com scanfold

--- nocoes_basicas_flutter_2/projeto_perguntas/lib/main.dart
+++ nocoes_basicas_flutter_2/projeto_perguntas/lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(PerguntaApp());
+void main(List<String> args) => runApp(PerguntaApp());
+void responder() {
+  print('Pergunta responddida!');
 }
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Text('Olar Mundo!'),
+        home: Scaffold(
+      appBar: AppBar(title: Text('Perguntas')),
+      body: Column(
+        children: [
+          Text(perguntas[0]),
+          Container(
+              child: RaisedButton(
+                  child: Text('Resposta 1'), onPressed: responder)),
+          Container(
+              child: RaisedButton(child: Text('Resposta 2'), onPressed: () {})),
+          Container(
+              child: RaisedButton(child: Text('Resposta 3'), onPressed: () {})),
+        ],
       ),
-    );
+    ));
   }
 }
