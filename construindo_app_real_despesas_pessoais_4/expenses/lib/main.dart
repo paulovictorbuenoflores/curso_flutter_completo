0f3adcc822df351fdd51535d91844497b11ff9bd
pv <paulovictorflores28@gmail.com>
2022-10-03 21:20:22 -0300

    mapeamento de objetos, que não sao objetos, sao transacoes convertido para elementos visuais para mostrar na tela usando a funcao map

--- construindo_app_real_despesas_pessoais_4/expenses/lib/main.dart
+++ construindo_app_real_despesas_pessoais_4/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) => runApp(ExpensesApp());
@@ -12,6 +13,21 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +35,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
@@ -28,8 +46,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           )
         ],
       ),
