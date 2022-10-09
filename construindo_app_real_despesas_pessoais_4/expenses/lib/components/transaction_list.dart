298f46a18e342dff24c03b38b0a803540f196fed
pv <paulovictorflores28@gmail.com>
2022-10-09 03:21:07 -0300

    deixando aplicacao modular, tirando a parte trasanction do main

---
+++ construindo_app_real_despesas_pessoais_4/expenses/lib/components/transaction_list.dart
@@ -0,0 +1,15 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
