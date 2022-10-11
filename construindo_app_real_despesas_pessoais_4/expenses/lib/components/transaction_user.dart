237240e80e071aa8d21ab1ee912ef7d976dcc46e
pv <paulovictorflores28@gmail.com>
2022-10-10 22:48:15 -0300

    Lista de rolagem SingleChildView

--- construindo_app_real_despesas_pessoais_4/expenses/lib/components/transaction_user.dart
+++ construindo_app_real_despesas_pessoais_4/expenses/lib/components/transaction_user.dart
@@ -27,6 +27,66 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 211.30,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
   ];
   _addTransaction(String title, double value) {
     final newTransaction = Transaction(
