2905d1934588a8ce4b4f0d4057d355b7cd36c6d2
pv <paulovictorflores28@gmail.com>
2022-11-29 14:48:50 -0300

    adicionando o chart_bar widget que compoe o nosso grafico das barrinhas

--- construindo_app_real_despesas_pessoais_4/expenses/lib/components/chart_bar.dart
+++ construindo_app_real_despesas_pessoais_4/expenses/lib/components/chart_bar.dart
@@ -3,12 +3,29 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ChartBar extends StatelessWidget {
-  final String label;
-
-  ChartBar(this.label);
+  final String? label;
+  final double? value;
+  final double? percentage;
+  ChartBar({
+    this.label,
+    this.value,
+    this.percentage,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Text('R\$${value?.toStringAsFixed(2)}'),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: null,
+        ),
+        SizedBox(height: 5),
+        Text(label!),
+      ],
+    );
   }
 }
