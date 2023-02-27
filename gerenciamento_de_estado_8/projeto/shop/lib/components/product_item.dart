8d2712fda16f12c7752ed0b245f125e7996ae62f
pv <paulovictorflores28@gmail.com>
2023-02-27 12:16:05 -0300

    usando o consumer, é util quando estamos modificando apenas um componente de forma especifica, isso acaba otimizando a interface grafica, gerando uma aplicacao mais fluida

--- gerenciamento_de_estado_8/projeto/shop/lib/components/product_item.dart
+++ gerenciamento_de_estado_8/projeto/shop/lib/components/product_item.dart
@@ -12,25 +12,25 @@ class ProductItem extends StatelessWidget {
 //  ProductItem(this.product);
   @override
   Widget build(BuildContext context) {
-    //final product = Provider.of<Product>(context, listen: true);
-
-    return Consumer<Product>(
-      builder: (ctx, product, _) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
-            onTap: () {
-              Navigator.of(context)
-                  .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
-            },
+    final product = Provider.of<Product>(context, listen: false);
+//usando o consumer de forma especifica, apenas onde o estado esta mudando, no caso nosso é no favorit.
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
               onPressed: () {
                 product.alternanciaFavorito();
               },
@@ -38,15 +38,15 @@ class ProductItem extends StatelessWidget {
                   product.isFavorite ? Icons.favorite : Icons.favorite_border),
               color: Theme.of(context).accentColor,
             ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.shopping_cart),
-              color: Theme.of(context).accentColor,
-            ),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
