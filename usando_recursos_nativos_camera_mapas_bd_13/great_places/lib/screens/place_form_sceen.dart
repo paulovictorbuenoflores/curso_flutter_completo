64e132585cdfbdf3226566ee4462b5ecc21c8361
pv <paulovictorflores28@gmail.com>
2023-04-11 22:48:42 -0300

    adicionando imagens

--- usando_recursos_nativos_camera_mapas_bd_13/great_places/lib/screens/place_form_sceen.dart
+++ usando_recursos_nativos_camera_mapas_bd_13/great_places/lib/screens/place_form_sceen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({Key? key}) : super(key: key);
@@ -10,10 +13,50 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+  File? _pickedImage;
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _submitForm() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Form')),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(this._selectImage),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          TextButton.icon(
+            icon: Icon(Icons.add),
+            // color: Theme.of(context).accentColor,
+            label: Text('Adicionar'),
+            //elevation: 0,
+            //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            onPressed: _submitForm,
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ usando_recursos_nativos_camera_mapas_bd_13/great_places/lib/widgets/image_input.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+  ImageInput(this.onSelectImage);
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+//_takePicture(){}
+  _tirarFoto() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile imageFile = await _picker.pickImage(
+        source: ImageSource.camera, maxWidth: 600) as XFile;
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    //widget.onSelectImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 180,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Text('Nenhuma Imagem!'),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            icon: Icon(Icons.camera),
+            label: Text('Tirar Foto'),
+            //  textColor: Theme.of(context).primaryColor,
+            onPressed: _tirarFoto,
+          ),
+        )
+      ],
+    );
+  }
+}
